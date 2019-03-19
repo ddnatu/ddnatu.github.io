03d1f6f81c4e0325f5004ac2f9da3fbb27a43880
Devdutta Natu <natudevdutta@gmail.com>
2019-03-20 01:08:13 +0530

    Copied to root folder

--- dist/App.d36a57b6.js
+++ dist/App.d36a57b6.js
@@ -42930,7 +42930,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56668" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61622" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
