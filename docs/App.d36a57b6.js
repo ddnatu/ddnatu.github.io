2de3d7d894e987301646311bfc5ce13a774e8671
Devdutta Natu <natudevdutta@gmail.com>
2019-03-20 00:34:03 +0530

    Docs Folder to Github

--- docs/App.d36a57b6.js
+++ docs/App.d36a57b6.js
@@ -42930,7 +42930,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57148" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56865" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
