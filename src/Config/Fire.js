8ac62a37020f2c12c26f0de0f08a78d822049c9f
Devdutta Natu <DNatu@zscaler.com>
2019-11-14 01:44:36 +0530

    Changing firebase account and news api integration to master

--- src/Config/Fire.js
+++ src/Config/Fire.js
@@ -1,14 +1,24 @@
 import firebase from 'firebase'
 
 // Initialize Firebase
+// var config = {
+//     apiKey: 'AIzaSyCXWQlyhWtAgHzs86aDDeezCWGVUNceock',
+//     authDomain: 'natukulvruttant-c6639.firebaseapp.com',
+//     databaseURL: 'https://natukulvruttant-c6639.firebaseio.com',
+//     projectId: 'natukulvruttant-c6639',
+//     storageBucket: '',
+//     messagingSenderId: '569988486477',
+// }
+
 var config = {
-    apiKey: 'AIzaSyCXWQlyhWtAgHzs86aDDeezCWGVUNceock',
-    authDomain: 'natukulvruttant-c6639.firebaseapp.com',
-    databaseURL: 'https://natukulvruttant-c6639.firebaseio.com',
-    projectId: 'natukulvruttant-c6639',
-    storageBucket: '',
-    messagingSenderId: '569988486477',
+    apiKey: 'AIzaSyAIyNREgabQAwu9I364QkwJvXTT3EnO0aY',
+    authDomain: 'natukulvruttant.firebaseapp.com',
+    databaseURL: 'https://natukulvruttant.firebaseio.com',
+    projectId: 'natukulvruttant',
+    storageBucket: 'natukulvruttant.appspot.com',
+    messagingSenderId: '112090006817',
 }
+
 const fire = firebase.initializeApp(config)
 
 export default fire
--- src/pages/News.js
+++ src/pages/News.js
@@ -1,14 +1,39 @@
 import React from 'react'
 import { Link } from '@reach/router'
+import axios from 'axios';
 
+const apiKey = '4fa0750590044d02b2fd3a2a3c407f39';
+const countryCodeIndia = 'in';
 class News extends React.Component {
-    state = {
-        news: true,
+    constructor(props) {
+        super(props)
+        this.state = {
+        }
+        this.getLatestFeed = this.getLatestFeed.bind(this)
     }
+
+    componentDidMount() {
+        this.getLatestFeed();
+    }
+
+    getLatestFeed() {
+        let res = axios.get(
+            `https://newsapi.org/v2/top-headlines?country=${countryCodeIndia}&apiKey=${apiKey}`
+        );
+        res.then((data) => {
+            console.log('response from the news api', data);
+        });
+        res.catch((error) => {
+            console.log('error', error);
+        });
+    }
+
+
     render() {
         return (
             <Link to={'/news/'} className="pet">
                 <div> News </div>
+
             </Link>
         )
     }
