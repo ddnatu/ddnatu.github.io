4a9c0375cdc8007cca01a6070ad5a0eb6b3dc6fa
Devdutta Natu <DNatu@zscaler.com>
2019-12-31 22:32:42 +0530

    few more stylistic changes to home modified

--- src/pages/ModifiedHome.js
+++ src/pages/ModifiedHome.js
@@ -1,58 +1,91 @@
 import React from 'react'
 import { Link } from '@reach/router'
-import { useFormik } from 'formik';
+//import { useFormik } from 'formik';
 
 import Carousel from './../pages/Carousel'
 import './../styles/homeModified.scss';
 
-class Modified extends React.Component {
+/* eslint-disable */
+const validEmailRegex = new RegExp(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i);
 
-    state = {
-        isModifiedHome: true
-    }
+const validateForm = (errors) => {
+    let valid = true;
+    Object.values(errors).forEach(
+        // if we have an error string set valid to false
+        (val) => val.length > 0 && (valid = false)
+    );
+    return valid;
+}
+
+class Modified extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            value: '',
+            username: null,
+            fullname: null,
+            email: null,
+            password: null,
+            errors: {
+                fullName: '',
+                email: '',
+                password: ''
+            }
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    };
+
+    // Newsletter Signup form handle function.
+    handleChange(event) {
+        event.preventDefault();
+        const { name, value } = event.target;
+        let errors = this.state.errors;
+        switch (name) {
+            case 'fullname':
+                errors.fullname =
+                    value.length < 5
+                        ? 'Full Name must be 5 characters long!'
+                        : '';
+                break;
+            case 'email':
+                errors.email =
+                    validEmailRegex.test(value)
+                        ? ''
+                        : 'Email is not valid!';
+                break;
+            default:
+                break;
+        }
+
+        this.setState({ errors, [name]: value }, () => {
+            console.log(errors);
+        })
+
+
+    }
+
+    handleSubmit(event) {
+        console.log('A name was submitted: ' + this.state.username + this.state.fullname + this.state.email);
+        event.preventDefault();
+        if (validateForm(this.state.errors)) {
+            console.info('Valid Form')
+        } else {
+            console.error('Invalid Form')
+        }
     };
 
     render() {
-
-        // Signup form handle function
-        const SignupForm = () => {
-            // Pass the useFormik() hook initial form values and a submit function that will
-            // be called when the form is submitted
-            const formik = useFormik({
-                initialValues: {
-                    email: '',
-                },
-                onSubmit: values => {
-                    alert(JSON.stringify(values, null, 2));
-                },
-            });
-            return (
-                <form onSubmit={formik.handleSubmit}>
-                    <label htmlFor="email">Email Address</label>
-                    <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        onChange={formik.handleChange}
-                        value={formik.values.email}
-                    />
-                    <button type="submit">Submit</button>
-                </form>
-            );
-        };
-
+        const { errors } = this.state;
         return (
-            <Link to={'/home-modified/'}>
+            <Link to={'/home-modified/'} >
                 <div className="home-container">
                     <div className="home-story">
                         <section id="carousel">
-                            <div className="content">
-                                <Carousel style={{ textAlign: 'center', marginTop: 100 }} />
-                            </div>
+                            <Carousel style={{ textAlign: 'center', marginTop: 100 }} />
                         </section>
+
                         <section id="purpose">
                             <div className="content">
                                 <h1 className="title">What is Natu Kulvruttant?</h1>
@@ -79,11 +112,31 @@ class Modified extends React.Component {
                                 <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there is not anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
                             </div>
                         </section>
+
                         <section id="newsletter">
-                            <div className="content">
-                                <h1 className="title">Daily Content</h1>
-                                <p>Subscribe to our newsletter!</p>
-                                <SignupForm />
+                            <div className="subscribeHeading">
+                                <span>Subscribe to our newsletter!</span>
+                            </div>
+                            <div className="subscriptionFormContainer">
+                                <form onSubmit={this.handleSubmit} className="subscriptionForm">
+                                    <label>
+                                        Name:
+                                     <input name="fullname" type="text" value={this.state.fullname} onChange={this.handleChange} />
+                                    </label>
+                                    {errors.fullName.length > 0 &&
+                                        <span className='error'>{errors.fullName}</span>}
+                                    <label>
+                                        Email:
+                                     <input
+                                            name="email"
+                                            type="email"
+                                            value={this.state.email}
+                                            onChange={this.handleChange} />
+                                    </label>
+                                    {errors.email.length > 0 &&
+                                        <span className='error'>{errors.email}</span>}
+                                    <button onClick={this.handleSubmit}>Submit</button>
+                                </form>
                             </div>
                         </section>
                         <section className="featured-quote wide dark">
